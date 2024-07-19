f0a46fd1889482679d7e915cc00a12f54ead2e3f
Mustafif <mustafif0929@gmail.com>
2024-07-19 18:49:28 -0400

    switched to shuttle

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@ pub mod cached;
 pub mod handles;
 pub mod projects;
 use handles::*;
-use rocket::{routes, Build, Rocket};
+use rocket::routes;
 use rocket_dyn_templates::Template;
-#[rocket::launch]
-async fn rocket() -> Rocket<Build> {
-    rocket::build()
-        .mount("/", routes![index, assets])
+
+#[shuttle_runtime::main]
+async fn main() -> shuttle_rocket::ShuttleRocket {
+    let rocket = rocket::build()
         .attach(Template::fairing())
+        .mount("/", routes![index, assets]);
+
+    Ok(rocket.into())
 }
-
-
-
