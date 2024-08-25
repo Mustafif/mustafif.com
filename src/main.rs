48098f1b091eb0b5d58c2eaf22905bcb3cdd71aa
Mustafif <mustafif0929@gmail.com>
2024-08-25 11:38:13 -0400

    added research interests and licenses

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,9 @@ use handles::*;
 use rocket::routes;
 use rocket_dyn_templates::Template;
 
-#[shuttle_runtime::main]
-async fn main() -> shuttle_rocket::ShuttleRocket {
-    let rocket = rocket::build()
+#[rocket::launch]
+async fn rocket() -> _ {
+    rocket::build()
         .attach(Template::fairing())
-        .mount("/", routes![index, assets]);
-
-    Ok(rocket.into())
+        .mount("/", routes![index, assets])
 }
